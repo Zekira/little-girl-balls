35a4d56b9505b22c3cef35c27665770909f31798
Atrufulgium <atrufulgium@gmail.com>
2017-05-26 19:13:26 +0200

    Animation base
    +The basis for animating sprites is in place

---
+++ code/Game Source/Assets/Scripts/Really Small Scripts/SpriteAnimator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimator : MonoBehaviour {
+
+    public Texture2D texture;
+    public int delay = 6;
+
+    private Sprite[] sprites;
+    private int currentFrame;
+    private int currentDelay = 0;
+    private SpriteRenderer spriteRenderer;
+
+    void OnEnable() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (texture != null) {
+            SetSprite(texture);
+        }
+    }
+
+    void Update() {
+        if (sprites != null) {
+            currentDelay--;
+            if (currentDelay <= 0) {
+                SetFrame(currentFrame + 1);
+                currentDelay = delay;
+            }
+        }
+    }
+
+    public Sprite[] SetSprite(Texture2D texture) {
+        int frames = texture.width/texture.height;
+        sprites = new Sprite[frames];
+        for (int i = 0; i < frames; i++) {
+            sprites[i] = Sprite.Create(texture, new Rect(i * texture.height, 0, texture.height, texture.height), Vector2.one * 0.5f);
+        }
+        return sprites;
+
+    }
+
+    public void SetSprites(Sprite[] sprites) {
+        this.sprites = sprites;
+    }
+
+    public void SetFrame(int frame) {
+        if (frame >= sprites.Length) {
+            frame -= sprites.Length;
+        }
+        currentFrame = frame;
+        spriteRenderer.sprite = sprites[frame];
+    }
+
+}
