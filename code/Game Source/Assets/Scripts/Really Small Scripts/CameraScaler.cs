71bb5842fef686be1903ae6bb7397e57449a2aa9
Atrufulgium <atrufulgium@gmail.com>
2017-06-24 10:58:42 +0200

    Allow resizable windows
    +Allow any resolution (except 0 width but who on earth would do that?)

---
+++ code/Game Source/Assets/Scripts/Really Small Scripts/CameraScaler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class CameraScaler : MonoBehaviour {
+
+    Camera gameCamera;
+
+	void Start () {
+        gameCamera = GetComponent<Camera>();
+        setCameraScale();
+	}
+    
+    void Update () {
+        setCameraScale();
+	}
+
+    void setCameraScale() {
+        if ((float) Screen.height / Screen.width < 0.75f) { //Bars on the side
+            gameCamera.orthographicSize = 5f; //Magic, don't touch
+        } else { //Bars on top (or no bars)
+            gameCamera.orthographicSize = 6.65f * Screen.height / Screen.width; //Magic, don't touch
+        }
+    }
+}
---
+++ code/Game Source/Assets/Scripts/Really Small Scripts/CanvasFitter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class CanvasFitter : MonoBehaviour {
+
+    Canvas canvas;
+    CanvasScaler scaler;
+
+    void Start () {
+        canvas = GetComponent<Canvas>();
+        scaler = canvas.GetComponent<CanvasScaler>();
+        fitCanvas();
+    }
+	
+	void Update () {
+        fitCanvas();
+    }
+
+    void fitCanvas() {
+        if (scaler == null) {
+            scaler = canvas.GetComponent<CanvasScaler>();
+        }
+        if ((float)Screen.height / Screen.width < 0.75f) { //Bars on the side
+            scaler.matchWidthOrHeight = 1f;
+        } else { //Bars on top (or no bars)
+            scaler.matchWidthOrHeight = 0f;
+        }
+    }
+}
