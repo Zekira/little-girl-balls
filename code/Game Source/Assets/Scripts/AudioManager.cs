5622e7a41443019ad3c7b66bd444ef900522fe8a
Atrufulgium <atrufulgium@gmail.com>
2017-08-07 13:20:21 +0200

    Moved some stuff away from the resource folder
    * Apparantly unity's own advice about their Resources folder is "don't use it"; it's memory intensive and adds to the loading time.
    I use the resources folder for almost everything.
    So at least I moved away the things that I don't load through Resources.Load to a Non-Resources folder.
    * While moving music from Resources to Non-Resources I remembered I still had to import Nakarin's title theme but forgot it had an extra 16 seconds of emptiness. Oh well we'll notice that and fix that sometime probably maybe.

---
+++ code/Game Source/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    //Set in inspector
+    public Transform thisTransform;
+    public AudioClip[] music = new AudioClip[16]; //0 = main menu; 2n-1, 2n = stage n music, stage n boss music for stages 1 through 7, 15 = credits music;
+
+    //TODO: EVERYTHING
+}
